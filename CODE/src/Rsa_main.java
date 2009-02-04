70e359cb2b4d34c5b9d0290cc714b5ea2e2eb7e7
TuononenP <petri.tuononen@students.turkuamk.fi>
2009-02-04 15:09:35 +0200

    Removed unused methods from RSA_main.

--- CODE/src/Rsa_main.java
+++ CODE/src/Rsa_main.java
@@ -58,20 +58,6 @@ public class Rsa_main {
     public byte[] encrypt(byte[] message, BigInteger e, BigInteger n) {       
         return (new BigInteger(message)).modPow(e, n).toByteArray();  
     } 
-    
-    /**
-     * Encrypts one letter that is already encoded to a number.
-     * 
-     * @param encoded One letter of message encoded to a number.
-     * @param e Public exponent.
-     * @param n Modulus.
-     * @return BigInteger Encrypted BigInteger.
-     */
-    public BigInteger encrypt(int encoded, BigInteger e, BigInteger n) {       
-    	BigInteger bIntMsg = new BigInteger(Integer.toString(encoded)); //int msg to BigInteger
-    	BigInteger c = bIntMsg.modPow(e, n); //encrypted
-    	return c;
-    }  
       
     /** 
      * Decrypt byte array 
@@ -80,19 +66,6 @@ public class Rsa_main {
      */  
     public byte[] decrypt(byte[] message, BigInteger d, BigInteger n) {  
         return (new BigInteger(message)).modPow(d, n).toByteArray();  
-    }  
-    
-    /**
-     * Decrypts one BigInteger (that represents one letter) to int. 
-     * 
-     * @param encrypted Encrypted number.
-     * @param d Private exponent.
-     * @param n	Modulus.
-     * @return int Decrypted int.
-     */
-    public int decrypt(BigInteger encrypted, BigInteger d, BigInteger n) {  
-    	BigInteger m = encrypted.modPow(d, n); //decrypted
-    	return Integer.parseInt(m.toString());
     }  
     
 //    /**
