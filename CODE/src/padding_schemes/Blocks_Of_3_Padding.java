74f6d654afcff34635184bbe9d35ae40397a2aa1
TuononenP <petri.tuononen@students.turkuamk.fi>
2009-02-13 21:46:40 +0200

    Major update Blocks of 3 padding scheme.

--- CODE/src/padding_schemes/Blocks_Of_3_Padding.java
+++ CODE/src/padding_schemes/Blocks_Of_3_Padding.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
  * Divides text into blocks of 3 letters.
  * Encodes text block to number.
  * Decodes number back to text block.
+ * Prints full encoding/decoding formulas from full phrases.
  * 
  * @author Petri Tuononen
  * @since 25.1.2009
@@ -27,81 +28,73 @@ public class Blocks_Of_3_Padding {
 	double dPart, dPart2, division, temp;
 	
 	/**
-	 * Constructor.
-	 */
-	public Blocks_Of_3_Padding() {
-	
-	}
-	
-	/**
-	 * Divide text into 3 letter blocks.
-	 * @param msg	Text to be divided into 3 letter blocks.
+	 * Divide plaintext into 3 letter blocks.
+	 * @param msg	Plaintext to be divided into 3 letter blocks.
 	 * @return		Table containing one 3 letter block per cell.
 	 */
-	public String[] DivideToBlocks(String message) {
-		String msg = message.toUpperCase();
-		String[] word_table;
-		String[] xWord_table;
-		String[] block_table;
-		String word;
-		String newWord;
-		int wordLength, tableLengt, remainder, threeLetterBlocks = 0;
-		//Divide message to words
-		word_table = msg.split(" ");
-		tableLengt = word_table.length;
-		xWord_table = new String[tableLengt];
-		//Test every word if they can be divided by 3
-		for (int i=0; i < word_table.length; i++) {
-			word = word_table[i];
-			wordLength = word.length();
-			remainder = wordLength%3;
-			if (remainder == 2) {
-				newWord = "X" + word;
-				xWord_table[i] = newWord;
-			}
-			else if (remainder == 1) {
-				newWord = "XX" + word;
-				xWord_table[i] = newWord;
-			}
-			else {
-				xWord_table[i] = word;
-			}
+	public String[] plaintextToBlocks(String message) {
+		//replace white spaces with x
+		message = message.replace(" ", "X");
+		
+		sB = new StringBuilder(message);
+		long length = sB.length();
+		
+		//calculate remainder when divided by three
+		int remainder = (int) (length%3);
+		//if remainder == 2 --> add x to the end of message
+		if (remainder == 2) {
+			sB.append("X");
 		}
-		//Define table size
-		block_table = new String[numOf3LetterBlocks(xWord_table)];
-		//Allocate each 3 letter block to one table cell
-		int k = 0, j = 0, alku = 0, loppu = 3;
-		for (int i=0; i < xWord_table.length; i++) {
-			j = 0;
-			alku = 0;
-			loppu = 3;
-			word = xWord_table[i];
-			//3 letter blocks per word
-			threeLetterBlocks = word.length()/3;
-			while (j < threeLetterBlocks) {
-				block_table[k+j] = word.substring(alku, loppu);
-				alku += 3;
-				loppu += 3;
-				j++;
-			}
-			k += threeLetterBlocks;
+		//if remainder == 1 --> add xx to the end message
+		if (remainder == 1) {
+			sB.append("XX");
 		}
-		return block_table;
+		
+		//get new length
+		length = sB.length();
+		
+		//divide phrase to blocks of three
+		String[] divided = new String[(int) (length/3)];
+		for (int start=0, end=3, i=0; end<=length; start+=3, end+=3, i++) {
+			divided[i] = sB.substring(start, end);
+		}
+		
+		return divided;
 	}
 	
 	/**
-	 * Returns the amount of three letter blocks in a table.
-	 * @param table String table to check.
-	 * @return amount of three letter blocks in a table.
+	 * Prints text in three letter blocks.
+	 * e.g. ATTACK AT SEVEN --> ATT ACK XAT XSE VEN
+	 * @param message Text in three letter blocks.
 	 */
-	public int numOf3LetterBlocks(String[] table) {
-		int amount = 0;
+	public void printBlockText(String message) {
+		String[] word_table = plaintextToBlocks(message);
 		String word;
-		for (int i=0; i < table.length; i++) {
-			word = table[i];
-			amount += word.length()/3;
+		for (int i=0; i < word_table.length; i++) {
+			word = word_table[i];
+			System.out.print(word+" ");
 		}
-		return amount;
+		System.out.println("");
+	}
+	
+	/**
+	 * Converts blocks of three letters to plaintext.
+	 * 
+	 * @param blocks Three letter blocks in a String array.
+	 * @return	Plaintext.
+	 */
+	public String blocksToPlaintext(String[] blocks) {
+		sB = new StringBuilder();
+		//append string array to stringbuilder
+		for (int i=0; i < blocks.length; i++) {
+			sB.append(blocks[i]);
+		}
+		String plaintext = sB.toString();
+		//replace x with white spaces
+		plaintext = plaintext.replace("X", " ");
+		//remove white spaces from the end
+		plaintext.trim();
+		return plaintext;
 	}
 	
 	/**
@@ -111,10 +104,10 @@ public class Blocks_Of_3_Padding {
 	 * @param text	 Three letter string.
 	 * @return Number of encoded text.
 	 */
-	public int encode(String text) {
-		int num = (int) (alphaNum.getNum(text.substring(0, 1))*Math.pow(BASE, 2)) +
-				  (int) (alphaNum.getNum(text.substring(1, 2))*Math.pow(BASE, 1)) +
-				  (int) (alphaNum.getNum(text.substring(2, 3))*Math.pow(BASE, 0));
+	public int encodeBlock(String block) {
+		int num = (int) (alphaNum.getNum(block.substring(0, 1))*Math.pow(BASE, 2)) +
+				  (int) (alphaNum.getNum(block.substring(1, 2))*Math.pow(BASE, 1)) +
+				  (int) (alphaNum.getNum(block.substring(2, 3))*Math.pow(BASE, 0));
 		return num;
 	}
 	
@@ -123,7 +116,7 @@ public class Blocks_Of_3_Padding {
 	 * @param text	Three letter string.
 	 * @return Mathematical formula.
 	 */
-	public String getFormula(String text) {
+	public String getEncodeFormula(String text) {
 		String formula;
 		setProductNumbers(text);
 		formula = products[0]+"*"+BASE+pow_two+" + "+products[1]+"*"+BASE+pow_one+" + "+products[2]+"*"+BASE+pow_zero;
@@ -135,7 +128,7 @@ public class Blocks_Of_3_Padding {
 	 * 
 	 * @param text Three letter text block.
 	 */
-	public void setProductNumbers(String text) {
+	private void setProductNumbers(String text) {
 		products[0] = alphaNum.getNum(String.valueOf(text.charAt(0)));
 		products[1] = alphaNum.getNum(String.valueOf(text.charAt(1)));
 		products[2] = alphaNum.getNum(String.valueOf(text.charAt(2)));
@@ -147,28 +140,14 @@ public class Blocks_Of_3_Padding {
 	 * @param text	Three letter string.
 	 * @return Whole mathematical formula.
 	 */
-	public String getFullFormula(String text) {
-		return text+" = "+getFormula(text)+" = "+encode(text);
-	}
-	
-	/**
-	 * Prints text in three letter blocks.
-	 * e.g. ATTACK AT SEVEN --> ATT ACK XAT XSE VEN
-	 * @param message Text in three letter blocks.
-	 */
-	public void printBlockText(String message) {
-		Blocks_Of_3_Padding padding = new Blocks_Of_3_Padding();
-		String[] word_table = padding.DivideToBlocks(message);
-		String word;
-		for (int i=0; i < word_table.length; i++) {
-			word = word_table[i];
-			System.out.print(word+" ");
-		}
+	public String getFullEncodeFormula(String text) {
+		return text+" = "+getEncodeFormula(text)+" = "+encodeBlock(text);
 	}
 	
 	/**
 	 * Rounds a number upwards to closest integer.
 	 * @param nro Number to round.
+	 * @param desPlace Defines number of desimals used to round. 
 	 * @return Rounded number.
 	 */
 	public double roundDouble(double nro, int desPlace) {
@@ -247,9 +226,7 @@ public class Blocks_Of_3_Padding {
 		else { //three products
 			//result after the first division is >26 so another division has to be done
 			System.out.println(encoded+" = ("+encoded+"/"+BASE+")*"+BASE);
-//			System.out.println(encoded+" = "+division+"*"+BASE);
 			System.out.println(encoded+" = "+roundDouble(division, 3)+"*"+BASE);
-//			System.out.println(encoded+" = ("+division+"/"+BASE+")*"+BASE+pow_two);
 			System.out.println(encoded+" = ("+roundDouble(division, 3)+"/"+BASE+")*"+BASE+pow_two);
 			division = division/BASE_double;
 			/*
@@ -257,20 +234,15 @@ public class Blocks_Of_3_Padding {
 			 * Text block 'ZZZ' has the biggest number when encoded (17575) and [17575/(26*26)] < 26.
 			 */
 			if (division < BASE) { //to be 100% sure that the division result is <26
-//				System.out.println(encoded+" = "+division+"*"+BASE+pow_two);
 				System.out.println(encoded+" = "+roundDouble(division, 3)+"*"+BASE+pow_two);
 				firstProduct = (int) division; //we get first product
 				dPart = division-firstProduct; //desimal part
-//				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+dPart+"*"+BASE+pow_two);
 				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+roundDouble(dPart, 3)+"*"+BASE+pow_two);
-//				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + ("+dPart+"*"+BASE+")*"+BASE);
 				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + ("+roundDouble(dPart, 3)+"*"+BASE+")*"+BASE);
 				temp = dPart*26;
-//				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+temp+"*"+BASE);
 				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+roundDouble(temp, 3)+"*"+BASE);
 				secondProduct = (int) temp;
 				dPart2 = temp - secondProduct;
-//				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+secondProduct+"*"+BASE+" + "+dPart2+"*"+BASE);
 				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+secondProduct+"*"+BASE+" + "+roundDouble(dPart2, 3)+"*"+BASE);
 				lastProduct = (int) roundDouble(BASE*dPart2, 1);
 				System.out.println(encoded+" = "+firstProduct+"*"+BASE+pow_two+" + "+secondProduct+"*"+BASE+" + "+lastProduct+"*"+BASE+pow_zero);
@@ -294,39 +266,72 @@ public class Blocks_Of_3_Padding {
 		System.out.println("Result: "+decoded);
 	}
 	
-	//prints encode & decode functions
-	public void testMethods(String block) {
-		System.out.println("Encode: ");
+	/**
+	 * Prints a full encoding part from one three letter block.
+	 * @param block
+	 */
+	public void printBlockEncode(String block) {
 		setProductNumbers(block);
 		System.out.println(alphaNum.getLetter(products[0])+"="+products[0]
 		             +", "+alphaNum.getLetter(products[1])+"="+products[1]
 		             +", "+alphaNum.getLetter(products[2])+"="+products[2]);
-		System.out.println(getFullFormula(block));
+		System.out.println(getFullEncodeFormula(block));
 		System.out.println("");
-		int encoded = encode(block);
-		System.out.println("Decode: ");
+	}
+	
+	/**
+	 * Prints a full decoding part from one number that represents a three letter block.
+	 * @param encoded
+	 */
+	public void printBlockDecode(int encoded) {
 		decode_print(encoded);
 	}
 	
 	/**
-	 * 
+	 * Print encoding from full phrase.
+	 * @param phrase Message to be encoded.
+	 */
+	public void printFullEncode(String phrase) {
+		//converts message to blocks of three
+		String[] msg = plaintextToBlocks(phrase);
+		//encode every block of three
+		System.out.println("Encode: ");
+		System.out.println("------------------------------------");
+		for (int i=0; i<msg.length; i++) {
+			printBlockEncode(msg[i]);
+		}
+	}
+	
+	/**
+	 * Print decoding from full number list.
+	 * @param decoded Numbers with white spaces to be decoded.
+	 */
+	public void printFullDecode(String decoded) {
+		System.out.println("Decode: ");
+		System.out.println("------------------------------------");
+		/*
+		 * 'String decoded' is numbers with spaces
+		 * Numbers are split into string array cells.
+		 */
+		String[] numbersStr = decoded.split(" ");
+		for (int i=0; i < numbersStr.length; i++) {
+			printBlockDecode(Integer.parseInt(numbersStr[i]));
+			System.out.println("");
+		}
+	}
+	
+	/**
+	 * Tests methods.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Blocks_Of_3_Padding pg = new Blocks_Of_3_Padding();
-		
-		pg.testMethods("ATT");
+
+		String msg = "ATTACK AT SEVEN";
+		System.out.println(msg);
+		pg.printBlockText(msg);
 		System.out.println("");
-		pg.testMethods("XSE");
-		
-		//Tests decode method
-//		System.out.println("///////////////////////////////////");
-//		System.out.println(pg.decode(513));
-//		System.out.println(pg.decode(62));
-//		System.out.println(pg.decode(15567));
-//		System.out.println(pg.decode(16020));
-//		System.out.println(pg.decode(14313));
-//		System.out.println("///////////////////////////////////");
-		//Test end
+		pg.printFullEncode(msg);
+		pg.printFullDecode("513 62 15567 16020 14313");
 	}
 }
