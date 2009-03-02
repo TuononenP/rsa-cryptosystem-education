92af243b82f47c77cee2b9962b44a9c38ca9dbd9
Jukatti <jukka.tuominen@students.turkuamk.fi>
2009-03-02 14:17:45 +0200

    Added Secure-methods.

--- CODE/src/padding_schemes/PaddingType2.java
+++ CODE/src/padding_schemes/PaddingType2.java
@@ -186,7 +186,108 @@ public class PaddingType2 {
 		s.append(deCode(integerMessage)+"\n");
 		return s.toString();
 	}	
+	
+	
+	/**
+	 * Returns whole enCode + enCrypt phase
+	 * @param msg String message to encrypt
+	 * @param exp BigInteger
+	 * @param mod BigInteger
+	 * @return String containing phases
+	 */
+	public String getEnCryptedSecure(String msg, BigInteger exp, BigInteger mod){
 		
+		StringBuilder s = new StringBuilder();
+		AlphabetNum alpha = new AlphabetNum();
+		EncryptDecrypt encrypter = new EncryptDecrypt();
+		
+		//--------------------- enCoding-------------------------
+		
+		ArrayList<Integer> numberMessage = enCode(msg);
+		/*s.append(msg+"\n\n");
+		s.append(alpha.getLetters()+"\n\n");
+		s.append("Type 2 encoded text:\n");*/
+//		for (Integer integer : numberMessage) {
+//			s.append(integer.toString()+" ");
+//		}
+		
+		//------------------------enCrypting-----------------------
+		
+		/*s.append("\n\nEncrypting:\n");
+		for (int i = 0; i < numberMessage.size(); i++) {
+		CalculationPhase phase = new CalculationPhase(new BigInteger(numberMessage.get(i).toString()), exp);
+		s.append(phase.getAll(mod));
+		s.append("\n\n");
+		}
+		s.append("Encryped: \n");*/
+		
+		BigInteger[] enCrypted = new BigInteger[numberMessage.size()];
+		String[] cryptotext = new String[numberMessage.size()];
+		for (int i = 0; i < enCrypted.length; i++) {
+			enCrypted[i]=(encrypter.encrypt(numberMessage.get(i),exp,mod));
+			cryptotext[i]=alpha.stringOfNumbersToLetters(enCrypted[i].toString());
+			//s.append(enCrypted[i]+" ");
+		}
+		//s.append("\n\n"+alpha.getNumbers());
+		//s.append("\n\nCryptotext:\n");
+		for (String string : cryptotext) {
+			s.append(string+" ");
+		}
+		return s.toString().trim();
+	}
+	
+	
+	
+	/**
+	 * Returns all phases of deCrypt and deCode
+	 * @param msg message
+	 * @param exp BigInteger
+	 * @param mod BigInteger
+	 * @return String
+	 */
+	public String getDeCryptedSecure(String msg, BigInteger exp, BigInteger mod){
+		StringBuilder s = new StringBuilder();
+		AlphabetNum alpha = new AlphabetNum();
+		EncryptDecrypt decrypter = new EncryptDecrypt();
+		
+		/*s.append("Cryptotext:\n");
+		s.append(msg+"\n\n");
+		s.append(alpha.getLetters()+"\n\n");*/
+		
+		//-----------cryptotext to numbers----------------------------
+		
+		msg=alpha.stringOfLettersToNumbers(msg);
+		String[] stringMessage = msg.split(" "); // Message in String[]
+		
+		//----------------------deCrypting--------------------------
+		
+		BigInteger[] numberMessage = new BigInteger[stringMessage.length];
+		
+		for (int i = 0; i < numberMessage.length; i++) {
+			numberMessage[i]=new BigInteger(stringMessage[i]);   // message to BigInteger[]	
+		}
+		/*s.append("Encrypted message: \n");
+		s.append(msg);
+		s.append("\n\nDecrypting: \n");
+		for (int i = 0; i < numberMessage.length; i++) {
+			CalculationPhase phase = new CalculationPhase(numberMessage[i],exp);
+			s.append(phase.getAll(mod));
+			s.append("\n\n");
+		}*/
+		//s.append("Decrypted: \n");
+	    ArrayList<Integer> integerMessage = new ArrayList<Integer>();
+		for (int i = 0; i < numberMessage.length; i++) {
+			integerMessage.add(i, decrypter.decryptToInt(numberMessage[i], exp, mod));
+			//s.append(integerMessage.get(i)+" ");
+		}
+		
+		//-----------------------deCoding-------------------------------
+		
+		/*s.append("\n\n"+alpha.getNumbers());
+		s.append("\n\nDecoded text: ");*/
+		s.append(deCode(integerMessage));
+		return s.toString().trim();
+	}	
 //------------------------main for testing----------------------------	
 
 	public static void main(String[] args) {
